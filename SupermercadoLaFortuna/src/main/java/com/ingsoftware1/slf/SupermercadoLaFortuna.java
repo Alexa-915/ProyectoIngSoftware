7d91f8c5b8bbb8d526294ae9a7db2e63136511ea
Alexa-915 <alexa.galeano@estudiantesunibague.edu.co>
2025-05-01 13:33:27 -0500

    Se creo el proyecto

---
+++ SupermercadoLaFortuna/src/main/java/com/ingsoftware1/slf/SupermercadoLaFortuna.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ingsoftware1.slf;
+
+/**
+ *
+ * @author alexa
+ */
+public class SupermercadoLaFortuna {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
